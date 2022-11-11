715fb5d9b2e4220cab4c03e6bafdc876f76e018c
Will Seagar <w.seagar@gmail.com>
2022-11-11 16:58:19 +1300

    First pass of adding chrome settings

---
+++ chrome/options.chrome.js
@@ -0,0 +1,69 @@
+function onTextEnabledChange() {
+  const textEnabled = document.getElementById("textEnabled").checked;
+  // hide the other text options if text is disabled
+  const elements = document.getElementsByClassName("text-option");
+  for (const elm of elements) {
+    elm.style.display = textEnabled ? "block" : "none";
+  }
+}
+
+function saveOptions() {
+  const memeMode = document.getElementById("memeMode").checked;
+  const textEnabled = document.getElementById("textEnabled").checked;
+  const textVerifiedLabel = document.getElementById("textVerifiedLabel").value;
+  const twitterBlueVerifiedLabel = document.getElementById(
+    "textTwitterBlueLabel"
+  ).value;
+  const textEnableBorder = document.getElementById("textEnableBorder").checked;
+  chrome.storage.sync.set(
+    {
+      memeMode,
+      textEnabled,
+      textOptions: {
+        textVerifiedLabel: textEnabled ? textVerifiedLabel : "",
+        twitterBlueVerifiedLabel: textEnabled ? twitterBlueVerifiedLabel : "",
+        enableBorder: textEnabled ? textEnableBorder : true
+      },
+    },
+    function () {
+      const status = document.getElementById("status");
+      status.textContent = "Options saved.";
+      setTimeout(function () {
+        status.textContent = "";
+      }, 750);
+    }
+  );
+}
+
+function restoreOptions() {
+  chrome.storage.sync.get(
+    {
+      memeMode: false,
+      textEnabled: true,
+      textOptions: {
+        verifiedLabel: "Verified",
+        twitterBlueLabel: "Paid",
+        enableBorder: true,
+      },
+    },
+    function (items) {
+      document.getElementById("memeMode").checked = items.memeMode;
+      document.getElementById("textEnabled").checked = items.textEnabled;
+      document.getElementById("textVerifiedLabel").value =
+        items.textOptions.verifiedLabel;
+      document.getElementById("textTwitterBlueLabel").value =
+        items.textOptions.twitterBlueLabel;
+      document.getElementById("textEnableBorder").checked =
+        items.textOptions.enableBorder;
+      onTextEnabledChange();
+    }
+  );
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  restoreOptions();
+  document
+    .getElementById("textEnabled")
+    .addEventListener("change", onTextEnabledChange);
+  document.getElementById("save").addEventListener("click", saveOptions);
+});
--- content.js
+++ content.js
@@ -1,6 +1,32 @@
-const s = document.createElement('script', { id: 'eight-dollars' });
-s.src = chrome.runtime.getURL('script.js');
-s.onload = function() {
-    this.remove();
+const defaultConfig = {
+  memeMode: false,
+  textEnabled: true,
+  textOptions: {
+    verifiedLabel: "Verified",
+    twitterBlueLabel: "Paid",
+    enableBorder: true
+  },
+};
+
+function createSettingsDomNode(items) {
+    const settingsDomNode = document.createElement("div");
+    settingsDomNode.id = "eight-dollars-settings";
+    settingsDomNode.style.display = "none";
+    settingsDomNode.innerText = JSON.stringify(items);
+    document.body.appendChild(settingsDomNode);
+}
+
+if (typeof chrome !== "undefined") {
+  chrome.storage.sync.get(defaultConfig, function (items) {
+    createSettingsDomNode(items);
+  });
+} else {
+    createSettingsDomNode(defaultConfig);
+}
+
+const s = document.createElement("script", { id: "eight-dollars" });
+s.src = chrome.runtime.getURL("script.js");
+s.onload = function () {
+  this.remove();
 };
 document.head.appendChild(s);
