d5767ef1af79afeec03dc1fdcf8ebe34b321de0b
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-11-12 16:51:52 +1300

    refactor more

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const REGULAR_BLUE_DOLLAR_SVG = `<svg width="24px" height="24px" viewBox="0 0 24
 <polygon id="Path" fill="#FFFFFF" fill-rule="nonzero" points="12.4306693 5.98530124 12.4306693 18.0146988 11.375459 18.0146988 11.375459 5.98530124"></polygon>
 </svg>`
 
-const REGULAR_BLUE_CHECK_SVG = `<svg viewBox="0 0 24 24" aria-label="Verified account" role="img" class="r-13v1u17 r-4qtqp9 r-yyyyoo r-1xvli5t r-f9ja8p r-og9te1 r-bnwqim r-1plcrui r-lrvibr"><g><path d="M22.25 12c0-1.43-.88-2.67-2.19-3.34.46-1.39.2-2.9-.81-3.91s-2.52-1.27-3.91-.81c-.66-1.31-1.91-2.19-3.34-2.19s-2.67.88-3.33 2.19c-1.4-.46-2.91-.2-3.92.81s-1.26 2.52-.8 3.91c-1.31.67-2.2 1.91-2.2 3.34s.89 2.67 2.2 3.34c-.46 1.39-.21 2.9.8 3.91s2.52 1.26 3.91.81c.67 1.31 1.91 2.19 3.34 2.19s2.68-.88 3.34-2.19c1.39.45 2.9.2 3.91-.81s1.27-2.52.81-3.91c1.31-.67 2.19-1.91 2.19-3.34zm-11.71 4.2L6.8 12.46l1.41-1.42 2.26 2.26 4.8-5.23 1.47 1.36-6.2 6.76z" style="fill: "#1D9BF0";"></path></g></svg>`;
+const REGULAR_BLUE_CHECK_SVG = `<svg viewBox="0 0 24 24" role="img" class="r-13v1u17 r-4qtqp9 r-yyyyoo r-1xvli5t r-f9ja8p r-og9te1 r-bnwqim r-1plcrui r-lrvibr"><g><path d="M22.25 12c0-1.43-.88-2.67-2.19-3.34.46-1.39.2-2.9-.81-3.91s-2.52-1.27-3.91-.81c-.66-1.31-1.91-2.19-3.34-2.19s-2.67.88-3.33 2.19c-1.4-.46-2.91-.2-3.92.81s-1.26 2.52-.8 3.91c-1.31.67-2.2 1.91-2.2 3.34s.89 2.67 2.2 3.34c-.46 1.39-.21 2.9.8 3.91s2.52 1.26 3.91.81c.67 1.31 1.91 2.19 3.34 2.19s2.68-.88 3.34-2.19c1.39.45 2.9.2 3.91-.81s1.27-2.52.81-3.91c1.31-.67 2.19-1.91 2.19-3.34zm-11.71 4.2L6.8 12.46l1.41-1.42 2.26 2.26 4.8-5.23 1.47 1.36-6.2 6.76z" style="fill: "#1D9BF0";"></path></g></svg>`;
 
 function changeVerified(elm, isSmall) {
   
@@ -35,9 +35,9 @@ function changeVerified(elm, isSmall) {
 </div>`;
   try {
     if (isSmall || !TEXT_ENABLED) {
-      elm.innerHTML = small;
+      elm.parentElement.innerHTML = small;
     } else {
-      elm.parentElement.parentElement.innerHTML = big;
+      elm.parentElement.innerHTML = big;
     }
   } catch (e) {
     console.log(elm);
@@ -47,7 +47,7 @@ function changeVerified(elm, isSmall) {
 
 function changeBlueVerified(elm, isSmall) {
   if (REMOVE_TWITTER_BLUE_VERIFICATION) {
-    elm.parentElement.parentElement.innerHTML = '';
+    elm.parentElement.innerHTML = '';
     return;
   }
 
@@ -60,9 +60,9 @@ function changeBlueVerified(elm, isSmall) {
     </div>`
   try {
     if (isSmall || !TEXT_ENABLED) {
-      elm.innerHTML = small;
+      elm.parentElement.innerHTML = small;
     } else {
-      elm.parentElement.parentElement.innerHTML = big;
+      elm.parentElement.innerHTML = big;
     }
   } catch (e) {
     console.log(elm);
@@ -145,9 +145,9 @@ function performBluecheckFindAndReplace(node) {
         nestedProps.isVerified;
 
       if (isBlueVerified) {
-        changeBlueVerified(blueCheckComponent.querySelector("g"), isSmall);
+        changeBlueVerified(blueCheckComponent, isSmall);
       } else if (isVerified) {
-        changeVerified(blueCheckComponent.querySelector("g"), isSmall);
+        changeVerified(blueCheckComponent, isSmall);
       }
     }
     catch (e) {
@@ -169,9 +169,9 @@ function performBluecheckFindAndReplace(node) {
       console.log({isBlueVerified, isVerified})
 
       if (isBlueVerified) {
-        changeBlueVerified(blueCheckEl.querySelector("g"), isSmall);
+        changeBlueVerified(blueCheckEl, isSmall);
       } else if (isVerified) {
-        changeVerified(blueCheckEl.querySelector("g"), isSmall);
+        changeVerified(blueCheckEl, isSmall);
       }
     } catch (e) {
       console.error("Error getting 'Provides details' react props: ", e)
