fc81d77cb74917461d5827057a11e460affa95c7
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-11-12 18:31:45 +1300

    check up the stack on whether the element exists

--- script.js
+++ script.js
@@ -26,11 +26,6 @@ const REGULAR_BLUE_DOLLAR_SVG = (isAriaLabel) => `<svg ${isAriaLabel ? 'aria-lab
 const REGULAR_BLUE_CHECK_SVG = `<svg viewBox="0 0 24 24" aria-label="Verified account" role="img" data-eight-dollars-status="verified" class="r-13v1u17 r-4qtqp9 r-yyyyoo r-1xvli5t r-f9ja8p r-og9te1 r-bnwqim r-1plcrui r-lrvibr"><g><path d="M22.25 12c0-1.43-.88-2.67-2.19-3.34.46-1.39.2-2.9-.81-3.91s-2.52-1.27-3.91-.81c-.66-1.31-1.91-2.19-3.34-2.19s-2.67.88-3.33 2.19c-1.4-.46-2.91-.2-3.92.81s-1.26 2.52-.8 3.91c-1.31.67-2.2 1.91-2.2 3.34s.89 2.67 2.2 3.34c-.46 1.39-.21 2.9.8 3.91s2.52 1.26 3.91.81c.67 1.31 1.91 2.19 3.34 2.19s2.68-.88 3.34-2.19c1.39.45 2.9.2 3.91-.81s1.27-2.52.81-3.91c1.31-.67 2.19-1.91 2.19-3.34zm-11.71 4.2L6.8 12.46l1.41-1.42 2.26 2.26 4.8-5.23 1.47 1.36-6.2 6.76z" style="fill: "#1D9BF0";"></path></g></svg>`;
 
 function changeVerified(elm, isSmall) {
-  if (!elm) {
-    // element no longer exists. TODO: check up the stack?
-    return
-  }
-
   if (elm.dataset.eightDollarsStatus === 'verified') {
     // already replaced this element
     return
@@ -54,11 +49,6 @@ function changeVerified(elm, isSmall) {
 }
 
 function changeBlueVerified(elm, isSmall) {
-  if (!elm) {
-    // element no longer exists. TODO: check up the stack?
-    return
-  }
-
   if (elm.dataset.eightDollarsStatus === 'blueVerified') {
     // already replaced this element
     return
@@ -176,10 +166,16 @@ function evaluateBlueCheckProvidesDetails() {
     try {
       const nestedProps = getReactProps(blueCheckComponent.parentElement.parentElement.parentElement.parentElement, blueCheckComponent)
 
+      const changeTarget = blueCheckComponent.querySelector('[aria-label="Verified account"]')
+
+      if (!changeTarget) {
+        throw new Error("Change target not found for a 'Provides details' node")
+      }
+
       if (nestedProps.isBlueVerified) {
-        changeBlueVerified(blueCheckComponent.querySelector('[aria-label="Verified account"]'), isSmall);
+        changeBlueVerified(changeTarget, isSmall);
       } else if (nestedProps.isVerified) {
-        changeVerified(blueCheckComponent.querySelector('[aria-label="Verified account"]'), isSmall);
+        changeVerified(changeTarget, isSmall);
       }
     } catch (e) {
       console.error("Error getting 'Provides details' react props: ", e)
