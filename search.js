cb516427db33a44344f01f1eae5d029c0db566ad
Walter Lim <waltissomewhere@gmail.com>
2022-11-23 20:50:33 +1300

    set up styling and layout

--- search.js
+++ search.js
@@ -2,12 +2,38 @@ function modifyDropdown(node) {
   const advancedSearch = `
   <div class='searchContainer'>
   <h4 class="searchContainerMemeHeading">Search Options</h4>   
-  <button class='searchItem'><strong>from:</strong> user</button>
-  <button class='searchItem'><strong>mentions:</strong> user</button>
-  <button class='searchItem'><strong>has:</strong> link, embed, or file</button>
-  <button class='searchItem'><strong>before:</strong> specific date</button>
-  <button class='searchItem'><strong>during:</strong> specific date</button>
-  <button class='searchItem'><strong>after:</strong> specific date</button>
+  <button id='searchUser' class='searchItem'><strong>from:</strong> user</button>
+  <button id='searchMentions' class='searchItem'><strong>mentions:</strong> user</button>
+  <button id='searchLinks' class='searchItem'><strong>has:</strong> link, embed, or file</button>
+  <button id='searchBefore' class='searchItem'><strong>before:</strong> specific date</button>
+  <button id='searchDuring' class='searchItem'><strong>during:</strong> specific date</button>
+  <button id='searchAfter' class='searchItem'><strong>after:</strong> specific date</button>
+  <style>
+  .searchContainer {
+    padding-bottom: 1rem;
+  }
+  .searchContainerMemeHeading {
+    color: white;
+    font-size: 20px;
+    font-family: TwitterChirp, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+    margin: 1rem;
+  }
+  .searchItem {
+    color: white;
+    font-size: 14px;
+    font-family: TwitterChirp, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, 
+    width: 100%;
+    display: block;
+    background: none;
+    border: none;
+    padding: 0.5rem 1rem;
+    transition: all 0.1s linear;
+  }
+
+  .searchItem:hover{
+    color: #1D9BF0;
+  }
+  </style>
   </div>
   `;
   node.innerHTML = `${advancedSearch}`;
