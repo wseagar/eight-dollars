2584c190126280cadc4316615643d4906b2a3662
Will Seagar <w.seagar@gmail.com>
2023-04-28 11:58:18 +1200

    Release v2.0
    Rewrite extension to make it work properly

--- content.js
+++ content.js
@@ -9,6 +9,13 @@ const defaultConfig = {
   },
 };
 
+async function loadVerifiedUsers() {
+  const url = chrome.runtime.getURL("data/verified.txt");
+  const response = await fetch(url);
+  const text = await response.text();
+  return text.split("\n");
+}
+
 function createSettingsDomNode(items) {
   const settingsDomNode = document.createElement("div");
   settingsDomNode.id = "eight-dollars-settings";
@@ -26,12 +33,28 @@ function injectScript() {
   document.head.appendChild(s);
 }
 
-if (typeof chrome !== "undefined" && chrome.storage) {
-  chrome.storage.local.get(defaultConfig, function (items) {
-    createSettingsDomNode(items);
-    injectScript();
-  });
-} else {
-  createSettingsDomNode(defaultConfig);
-  injectScript();
+function injectVerifiedUsers(verifiedUsers) {
+  const verifiedUsersDomNode = document.createElement("div");
+  verifiedUsersDomNode.id = "eight-dollars-verified-users";
+  verifiedUsersDomNode.style.display = "none";
+  verifiedUsersDomNode.innerText = JSON.stringify(verifiedUsers);
+  document.body.appendChild(verifiedUsersDomNode);
 }
+
+async function main() {
+  const verifiedUsers = await loadVerifiedUsers();
+
+  if (typeof chrome !== "undefined" && chrome.storage) {
+    chrome.storage.local.get(defaultConfig, function (items) {
+      createSettingsDomNode(items);
+      injectScript();
+      injectVerifiedUsers(verifiedUsers);
+    });
+  } else {
+    createSettingsDomNode(defaultConfig);
+    injectScript();
+    injectVerifiedUsers(verifiedUsers);
+  }
+}
+
+main();
